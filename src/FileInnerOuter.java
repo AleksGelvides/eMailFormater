b73a2d6285ddab82b89a8fd3a9b8f44bbc292d58
AlekseyGelvides <gelvides.aleksey@outlook.com>
2021-07-19 22:41:31 +0700

    Работает как часы :)

---
+++ src/FileInnerOuter.java
@@ -0,0 +1,52 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileInnerOuter {
+
+    public static void copyAndFormating(String filename, String outDir, String outFileName) throws IOException {
+        String mNoFormat = inputFiles(filename);
+        outputFiles(formater(mNoFormat), outDir, outFileName);
+    }
+
+    private static String inputFiles(String fileName) throws IOException {
+        String mails = "";
+        FileInputStream fis = new FileInputStream(fileName);
+        int i;
+        while ((i = fis.read()) != -1){
+            mails += String.valueOf((char)i);
+        }
+        fis.close();
+        return mails;
+    }
+
+    private static String[] formater(String mNoFormat){
+        Pattern p = Pattern.compile(".*?(\\Wru|\\Wcom|\\Wua)");
+        Matcher mLenght = p.matcher(mNoFormat);
+        int lenght= 0;
+        while(mLenght.find()){
+            lenght++;
+        }
+        String[] mails = new String[lenght];
+        Matcher mMail = p.matcher(mNoFormat);
+        for (int i = 0; i < mails.length; i++){
+            if(mMail.find()){
+                int start = mMail.start();
+                int end = mMail.end();
+                mails[i] = mNoFormat.substring(start, end);
+            }
+        }
+        return mails;
+    }
+
+    private static void outputFiles(String[] arr, String outDir, String fileName) throws IOException{
+        String puth = outDir.concat(fileName);
+        FileOutputStream fos = new FileOutputStream(fileName);
+        for(int i = 0; i < arr.length; i++){
+            fos.write(arr[i].toString().getBytes());
+            fos.write("\n".getBytes());
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileInnerOuter.copyAndFormating("EmailBase.txt", "", "test1.txt");
+    }
+}
